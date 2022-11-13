296ddc840b793471222e5405fa8b339099f527ee
Hemani Gajjar <hmngjjr17@gmail.com>
2022-11-13 13:06:47 +0530

    update settings.py for django admin missing css

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -122,8 +122,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+import os.path
+import sys
+PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
