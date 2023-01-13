00faac69fba869fde73e39fb67c9871733f59be5
Hemani Gajjar <hmngjjr17@gmail.com>
2023-01-13 13:13:32 +0530

    add home and genes page

---
+++ frontend/src/pages/home/About.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function About() {
+  return (
+      <div className='about-page'>
+        <div>Gene–environment interaction is when two different genotypes respond to environmental variation in different ways. A norm of reaction is a graph that shows the relationship between genes and environmental factors when phenotypic differences are continuous. They can help illustrate GxE interactions. When the norm of reaction is not parallel, as shown in the figure below, there is a gene by environment interaction. This indicates that each genotype responds to environmental variation in a different way. Environmental variation can be physical, chemical, biological, behavior patterns or life events.</div>
+
+
+      <div>
+      This norm of reaction shows lines that are not parallel indicating a gene by environment interaction. Each genotype is responding to environmental variation in a different way.
+      Gene–environment interactions are studied to gain a better understanding of various phenomena. In genetic epidemiology, gene–environment interactions are useful for understanding some diseases. Sometimes, sensitivity to environmental risk factors for a disease are inherited rather than the disease itself being inherited. Individuals with different genotypes are affected differently by exposure to the same environmental factors, and thus gene–environment interactions can result in different disease phenotypes. For example, sunlight exposure has a stronger influence on skin cancer risk in fair-skinned humans than in individuals with darker skin.
+      </div>
+
+      <div>
+        This website collects the data from the following sources as of now: 
+      <div> 1. <a href='https://www.snpedia.com/index.php/SNPedia' target="_blank" rel="noopener noreferrer">SNPedia</a></div> 
+      <div> 2. <a href='https://pubmed.ncbi.nlm.nih.gov/' target="_blank" rel="noopener noreferrer">PubMed</a> </div>
+       
+      </div>
+    </div>
+  )
+}
